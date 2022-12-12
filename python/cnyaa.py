f406b235f17ccc5e6e63ec18ae6e981b35052c3b
Preben <84629067+PrebenL@users.noreply.github.com>
2022-12-12 20:31:27 +0100

    Update cnyaa.py
    Revamp

--- python/cnyaa.py
+++ python/cnyaa.py
@@ -1,7 +1,6 @@
-import requests
-import json
-import extractModule as em
-from bs4 import BeautifulSoup
+#!/usr/bin/env python3
+
+import json, extract as em
 from termcolor import colored, cprint
 
 vSeedDescUrl = "https://nyaa.si/?s=seeders&o=desc"
@@ -11,19 +10,6 @@ vWantedList = [
 'Spy x Family'
 ]
 
-def returnFirstPage():
-	for item in vSoupCall.find_all('td', {'colspan': '2'}):
-		for x in item.find_all('a', href=True):
-			vTitle = x.get('title', 'No title attribute')
-			vTitle = re.sub(r'\[.+?\]', '', vTitle)
-			if "comment" not in vTitle:
-				if "1080p" in vTitle:
-					cprint(colored(vTitle, 'yellow', attrs=["bold", "underline"]))
-				else:
-					cprint(colored(vTitle, 'blue'))
-
 def writeResult(pJson, filename):
 	with open("{}.json".format(filename), "w") as output:
 		json.dump(pJson, output, indent=1)
-
-writeResult(em.extractWantedAnimeMagnetLinks(5, BeautifulSoup(requests.get(vSeedDescUrl).text, 'lxml'), vWantedList), "test_002")
